e5e2dc53040b03647f7dcb9d4513b7482e15e58f
shotaro takizawa <taki21zawa@gmail.com>
2021-03-12 14:51:12 +0900

    徹底攻略　JAVA SE11 Silver　3章

---
+++ silver_se11/js11/src/js11/Chapter3.java
@@ -0,0 +1,60 @@
+package js11;
+
+public class Chapter3 {
+
+  public static void main(String[] args) {
+    // 1
+    {
+      int a = 3;
+      int b = a += 5;
+      System.out.println("[1]");
+      System.out.println(a + b);
+      System.out.println("-----------");
+    }
+
+    // 2
+    {
+      int num = -10;
+      System.out.println("[2]");
+      System.out.println(10 * -num);
+      System.out.println("-----------");
+    }
+
+    // 3
+    {
+      //      byte a = 0b10000000;
+      short b = 128 + 128;
+      //      int c = 2 * 3L;
+      //      float d = 10.0;
+    }
+
+    // 4
+    {
+      int a = 10;
+      int b = a++ + a + a-- - a-- + ++a;
+      System.out.println("[4]");
+      System.out.println(b);
+      System.out.println("-----------");
+    }
+
+    // 5
+    {
+      boolean a = true;
+      boolean b = true;
+      System.out.println("[5]");
+      //      System.out.println(a <= b);
+      System.out.println("-----------");
+
+    }
+
+    //
+    {
+
+      System.out.println("[]");
+      System.out.println();
+      System.out.println("-----------");
+
+    }
+  }
+
+}
