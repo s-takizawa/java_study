2bab6cc847c15d8f3e781cb5d0f43ae4c3c58980
s.takizawa <taki21zawa@gmail.com>
2021-03-15 16:49:41 +0900

    徹底攻略　JAVA SE11 Silver　リストの固定長関連を追加

--- silver_se11/js11/src/js11/Chapter3.java
+++ silver_se11/js11/src/js11/Chapter3.java
@@ -1,5 +1,9 @@
 package js11;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Chapter3 {
 
   public static void main(String[] args) {
@@ -8,6 +12,15 @@ public class Chapter3 {
 
       System.out.println("[その他]");
       System.out.println(1 + 2 + "3" + 4 + 5);
+      var list1 = List.of(1, 2, 3); // List<Integer>:固定長
+      System.out.println(list1);
+      var list2 = new ArrayList<Integer>(); // ArrayList<Integer>:可変長
+      System.out.println(list2);
+      var list3 = Arrays.asList(new Integer[] { 1, 2, 3 }); // List<Integer>:固定長
+      System.out.println(list3);
+      var list4 = new ArrayList<Integer>(3); // ArrayList<Integer>:可変長
+      System.out.println(list4);
+
       System.out.println("-----------");
 
     }
@@ -51,6 +64,46 @@ public class Chapter3 {
       boolean b = true;
       System.out.println("[5]");
       //      System.out.println(a <= b);
+      System.out.println("-----------");
+
+    }
+
+    // 9-17
+    {
+
+      System.out.println("[9-17]");
+
+      List<String> list = new ArrayList<>(
+          Arrays.asList(new String[] { "A", "B", "C" }));
+      list.removeIf((String s) -> {
+        return s.equals("B");
+      });
+      System.out.println(list);
+      System.out.println();
+
+      // 以下は実行時エラー UnsupportedOperationException
+      try {
+        List<String> list2 = Arrays.asList(new String[] { "A", "B", "C" });
+        list2.removeIf((String s) -> {
+          return s.equals("B");
+        });
+        System.out.println(list2);
+      } catch (UnsupportedOperationException e) {
+        // TODO: handle exception
+        System.out.println(e);
+      }
+      try {
+        List<String> list3 = List.of("A", "B", "C");
+        list3.removeIf((String s) -> {
+          return s.equals("B");
+        });
+        System.out.println(list3);
+      } catch (UnsupportedOperationException e) {
+        // TODO: handle exception
+        System.out.println(e);
+      }
+      System.out.println();
+
       System.out.println("-----------");
 
     }
